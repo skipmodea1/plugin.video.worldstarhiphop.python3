123d3b1b6cc1d600c6675f81e07f9c350ca8945b
skipmodea1 <skipmodea1@outlook.com>
2020-10-25 03:58:48 +0100

    v1.0.13 (2030-10-25)
    - changes due to website changes

--- addon.py
+++ addon.py
@@ -13,7 +13,7 @@ import sys
 import urllib.parse
 import xbmc
 
-from resources.worldstarhiphop_const import ADDON, DATE, VERSION, BASEURL
+from resources.lib.worldstarhiphop_const import ADDON, DATE, VERSION, BASEURL
 
 # Parse parameters...
 if len(sys.argv[2]) == 0:
